735b46284de815deff3ec5487373e7aa5bcbec41
angeltorx <angelfitorres@gmail.com>
2023-06-26 21:13:55 -0500

    6commits

--- src/acquisitions.js
+++ src/acquisitions.js
@@ -1,24 +1,27 @@
 import Chart from "chart.js/auto";
 import { incrementarValor } from "./funciones";
+import { getAquisitionsByYear } from './api'
 
 (async function () {
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ].map(row=>({...row, count:incrementarValor(row.count) }));
+//  const data = [
+//  { year: 2010, count: 10 },
+//  { year: 2011, count: 20 },
+//  { year: 2012, count: 15 },
+//  { year: 2013, count: 25 },
+//  { year: 2014, count: 22 },
+//  { year: 2015, count: 30 },
+//  { year: 2016, count: 28 },
+//].map(row=>({...row, count:incrementarValor(row.count) }));
+
+const data = await getAquisitionsByYear();
 
   new Chart(document.getElementById("acquisitions"), {
-    type: "bar",
+    type: "line",
     data: {
       labels: data.map((row) => row.year),
       datasets: [
         {
-          label: "Acquisitions by year",
+          label: "Ventas por año",
           data: data.map((row) => row.count),
         },
       ],
